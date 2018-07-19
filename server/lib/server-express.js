70ecbfc7e72be517fc1f8a54d3ae3b5a904d51d2
Sanjaysinh Zala <sanjaysinhz@officebrain.com>
2018-07-19 09:33:51 +0530

    Done express framework  implementation

--- server/lib/server-express.js
+++ server/lib/server-express.js
@@ -8,44 +8,65 @@ var _express = require('express');
 
 var _express2 = _interopRequireDefault(_express);
 
+var _ejs = require('ejs');
+
+var _ejs2 = _interopRequireDefault(_ejs);
+
 var _bodyParser = require('body-parser');
 
 var _bodyParser2 = _interopRequireDefault(_bodyParser);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-var server = (0, _express2.default)();
 var PORT = process.env.PORT || 5000;
-
-server.use(_bodyParser2.default.json());
+var server = (0, _express2.default)();
+server.set('view engine', 'ejs');
+server.use(_bodyParser2.default.json()); // for parsing application/json
+server.use(_bodyParser2.default.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
 var bot = void 0;
 
 server.get('/superscript', function (req, res) {
-  if (req.query.message) {
-    return bot.reply('user1', req.query.message, function (err, reply) {
-      res.json({
-        message: reply.string
-      });
-    });
+		res.render('index', { title: 'Chatbot', text: 'Welcome to the SuperScript  Demo!\n' });
+		/* if (req.query.message) {
+  		return bot.reply('user1', req.query.message, (err, reply) => {
+  				res.json({
+  						message: reply.string,
+  				});
+  		});
   }
-  return res.json({ error: 'No message provided.' });
+   return res.json({ error: 'No message provided.' }); */
+});
+
+server.post('/chat-message', function (req, res) {
+		if (req.body.message) {
+				return bot.reply('newUser', req.body.message.trim(), function (err, reply) {
+						console.log('bot reply', { text: reply.string, type: reply.action, date: reply.date });
+						var color = reply.color || "#fff";
+						var msg = { text: reply.string, type: reply.action, date: reply.date };
+						for (var key in reply) {
+								if (key !== 'text' && key !== 'action' && key !== 'date') msg[key] = reply[key];
+						}
+						res.send(msg);
+				});
+		}
+		return res.send('No message provided.');
 });
 
 var options = {
-  factSystem: {
-    clean: true
-  },
-  importFile: './data.json'
+		factSystem: {
+				clean: true
+		},
+		importFile: './data.json'
 };
 
 _superscript2.default.setup(options, function (err, botInstance) {
-  if (err) {
-    console.error(err);
-  }
-  bot = botInstance;
+		if (err) {
+				console.error(err);
+		}
+		bot = botInstance;
 
-  server.listen(PORT, function () {
-    console.log('===> \uD83D\uDE80  Server is now running on port ' + PORT);
-  });
+		server.listen(PORT, function () {
+				console.log('===> \uD83D\uDE80  Server is now running on port ' + PORT);
+		});
 });
--- server/src/server-express.js
+++ server/src/server-express.js
@@ -1,30 +1,49 @@
 import superscript from 'superscript';
 import express from 'express';
+import ejs from 'ejs'
 import bodyParser from 'body-parser';
 
-const server = express();
 const PORT = process.env.PORT || 5000;
-
-server.use(bodyParser.json());
+const server = express();
+server.set('view engine', 'ejs');
+server.use(bodyParser.json());  // for parsing application/json
+server.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
 let bot;
 
 server.get('/superscript', (req, res) => {
-  if (req.query.message) {
-    return bot.reply('user1', req.query.message, (err, reply) => {
-      res.json({
-        message: reply.string,
-      });
-    });
-  }
-  return res.json({ error: 'No message provided.' });
+	res.render('index', {title: 'Chatbot', text:'Welcome to the SuperScript  Demo!\n'});
+	/* if (req.query.message) {
+			return bot.reply('user1', req.query.message, (err, reply) => {
+					res.json({
+							message: reply.string,
+					});
+			});
+	}
+  return res.json({ error: 'No message provided.' }); */
 });
 
+server.post('/chat-message', (req, res) => {    
+	if (req.body.message) {
+		return bot.reply('newUser', req.body.message.trim(), (err, reply) => {
+			console.log('bot reply', { text: reply.string, type: reply.action, date: reply.date });
+			let color = reply.color || "#fff";
+			let msg = { text: reply.string, type: reply.action, date: reply.date }
+			for (var key in reply) {
+				if(key!=='text' && key!=='action' && key!=='date')
+					msg[key]=reply[key]
+			}
+			res.send(msg);
+		});
+	}
+	return res.send('No message provided.');
+})
+
 const options = {
-  factSystem: {
-    clean: true,
-  },
-  importFile: './data.json',
+    factSystem: {
+        clean: true,
+    },
+    importFile: './data.json',
 };
 
 superscript.setup(options, (err, botInstance) => {
